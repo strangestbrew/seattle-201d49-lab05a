b176fbfd416f9def60bcafd12858f457c5399f6b
strangestbrew <thomas.lyndsey@gmail.com>
2019-06-09 22:04:15 -0700

    changes to question 4

--- app.js
+++ app.js
@@ -29,35 +29,37 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sumAndMultiply(a, b, c) {
   var sumFuncThree = a + b + c; 
-  return [sumFuncThree, "The sum of " + a + "&" + b + "&" + c + " is " + sumFuncThree];
-  &
+  return [sumFuncThree, "The sum of " + a + " & " + b + " & " + c + " is " + sumFuncThree];
+  
   var multiplyFuncThree = a*b*c;
-  return [multiplyFuncThree + "The product of " + a + "&" + b + "&" + c + " is " + multiplyFuncThree];
+  return [multiplyFuncThree, "The product of " + a + " & " + b + " & " + c + " is " + multiplyFuncThree];
 }
 
 console.log(sumAndMultiply(4, 7, 5));
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+//Question 4
+var testArray = [2, 3, 4]; 
 
-/////////////////////////////////////
-/* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To do addition, use your sum() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
-
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
-
-// Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
-
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) { 
+  return testArray; 
+  const arrSum = arr => arr.reduce((a,b) => a + b, 0);
+ //function sum(a, b, c); 
 
 }
+
+console.log(testArray);
+
+var testArray = [2, 3, 4]; 
+
+function sumArray(2,3,4) { 
+  for (i = 0; i < testArray.length; i++);
+  sum += arr[i];
+  return sum;
+  //return testArray; 
+   //function sum(a, b, c); 
+}
+console.log(sumArray);
 
 // Here is the test for sumArray(); uncomment it to run it
 
